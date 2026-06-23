036386823ef1d0efe36b0ab77ad5012554a45d3e
haseem <hasehaseem04@gmail.com>
2026-06-23 11:18:06 +0300

    bitmoji changed

--- app/Components/HeroSection.tsx
+++ app/Components/HeroSection.tsx
@@ -25,7 +25,7 @@ export default function HeroSection() {
           {/* Bitmoji Head overlapping the text */}
           <div className="absolute top-[35%] sm:top-[40%] md:top-[45%] left-1/2 -translate-x-1/2 w-[400px] sm:w-[520px] md:w-[600px] lg:w-[680px] aspect-square z-10 animate-float pointer-events-none">
             <Image
-              src="/assets/Images/bitmoji.png"
+              src="/assets/Images/myBitmoji.png"
               alt="Alex Bitmoji"
               fill
               priority
