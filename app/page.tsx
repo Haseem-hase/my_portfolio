619b7ec004b85d127e409a0229e0aa965a36c66c
haseem <hasehaseem04@gmail.com>
2026-06-23 11:37:41 +0300

    dome gallery added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import HeroSection from './Components/HeroSection'
+import Gallery from './Components/Gallery'
 
 const page = () => {
   return (
     <main className="min-h-screen bg-black">
       <HeroSection />
+      {/* 3D Dome Gallery Section */}
+      <section className="relative w-full h-screen bg-black flex flex-col justify-start items-center overflow-hidden pt-16">
+        {/* Gallery Section Heading */}
+        <div className="z-20 flex flex-col items-center justify-center text-center px-4 pb-6">
+
+          <h2 className="font-display text-3xl sm:text-4xl md:text-5xl font-black tracking-tight leading-none silver-gradient-text uppercase">
+            Deep Into My Memories
+          </h2>
+
+        </div>
+
+        {/* Dome Gallery Container */}
+        <div className="relative w-full flex-1 min-h-0">
+          <Gallery />
+        </div>
+      </section>
     </main>
   )
 }
